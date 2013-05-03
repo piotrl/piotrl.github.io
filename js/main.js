fc563b3c7917b28dbc8b55608450aaac3ed4ea4d
Piotr Lewandowski <grovman@gmail.com>
2013-05-03 23:23:38 +0200

    Navigation of gallery slides

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ portfolio = {
     settings: {
         navList: document.querySelectorAll('.navigate nav a'),
         projectList: document.querySelectorAll('section.works a.thumb'),
+        galleryEl: document.getElementById('presentation'),
         scrollTime: 500
     },
     init: function() {
@@ -20,6 +21,7 @@ portfolio = {
         };
 
         portfolio.bindUIActions();
+        portfolio.createGalleryNav();
     },
     bindUIActions: function() {
         [].forEach.call(s.navList, function(that) {   // that: Node Element of actual iteration
@@ -27,6 +29,7 @@ portfolio = {
                 direction = direction.offsetTop;                 // and check his offset
 
             that.addEventListener('click', function(event) {
+                portfolio.removeGalleryLayer();
                 portfolio.setHash(that);
                 portfolio.scrollTo(direction, s.scrollTime);
 
@@ -36,17 +39,15 @@ portfolio = {
         });
 
         [].forEach.call(s.projectList, function(that) {
-            that.addEventListener('click', function(){
+            that.addEventListener('click', function(event){
                 document.documentElement.classList.add("presentation");
+
+                event.preventDefault();
             }, false);
         });
     },
     // setters
     setHash: function(that) {
-        if( document.documentElement.classList.contains('presentation') ) {
-            document.documentElement.classList.remove("presentation");
-        }
-
         [].forEach.call(s.navList, function(that) {
             that.classList.remove("active");        // Remove '.active' from every nav position
         });
@@ -57,6 +58,56 @@ portfolio = {
         } else { /* fallback */
             location.hash = that.hash;
         }
+    },
+    createGalleryNav: function() {
+        // create 'close' element
+        var closeGalleryEl = document.createElement('a'),
+            nextGalleryEl = closeGalleryEl.cloneNode(),
+            prevGalleryEl = closeGalleryEl.cloneNode();
+
+        closeGalleryEl.classList.add('close-button');
+        nextGalleryEl.classList.add('next-button');
+        prevGalleryEl.classList.add('prev-button');
+
+
+        s.galleryEl.appendChild(nextGalleryEl);
+        s.galleryEl.appendChild(prevGalleryEl);
+        s.galleryEl.appendChild(closeGalleryEl);
+
+         // add new events
+        closeGalleryEl.addEventListener('click', portfolio.removeGalleryLayer, false);
+        nextGalleryEl.addEventListener('click', portfolio.slideNext, false);
+        prevGalleryEl.addEventListener('click', portfolio.slidePrev, false);
+    },
+    removeGalleryLayer: function() {
+        if( document.documentElement.classList.contains('presentation') ) {
+            document.documentElement.classList.remove("presentation");
+        }
+        console.log('gallery layer');
+    },
+    slideNext: function() {
+        var current = document.querySelector('img.current'),
+            next = document.querySelector('img.next');
+
+            if(next) {
+                current.classList.remove('current');
+                current.classList.add('prev');
+
+                next.classList.remove('next');
+                next.classList.add('current');
+            }
+    },
+    slidePrev: function() {
+        var current = document.querySelector('img.current'),
+            prev = document.querySelector('img.prev');
+
+            if(prev) {
+                current.classList.remove('current');
+                current.classList.add('next');
+
+                prev.classList.remove('prev');
+                prev.classList.add('current');
+            }
     },
     scrollTo: function(to, duration) {
             // element = document.body for Chrome, and document.documentElement for firefox
